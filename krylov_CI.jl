afad56ef96c1b581b4e9d2d973d7d80a4173be35
Monssaf Toukal <tmonssaf@gmail.com>
2020-06-17 14:18:45 -0400

    correcting LearnBase issue

--- krylov_CI.jl
+++ krylov_CI.jl
@@ -1,7 +1,8 @@
 using Pkg
 Pkg.activate("./jenkins_env/")
 
-Pkg.add(["PkgBenchmark", "BenchmarkTools", "MatrixDepot", "MatrixMarket", "GitHub", "JSON", "LinearOperators"])
+Pkg.add(["PkgBenchmark", "BenchmarkTools", "MatrixDepot", "MatrixMarket", "GitHub", "JSON", "LinearOperators", "LearnBase"])
+Pkg.pin(PackageSpec(name="LearnBase", version="0.3"))
 Pkg.develop(PackageSpec(path="./Krylov.jl"))
 Pkg.update()
 
