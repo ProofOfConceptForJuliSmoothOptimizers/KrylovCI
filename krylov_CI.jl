18ac439de6bbcfad2fd54f8a772da91f86d11639
Monssaf Toukal <tmonssaf@gmail.com>
2020-06-18 14:15:35 -0400

    disabling fetch_ssmc

--- krylov_CI.jl
+++ krylov_CI.jl
@@ -11,7 +11,7 @@ using SuiteSparseMatrixCollection
 
 
 ufl_posdef = filter(p -> p.structure == "symmetric" && p.posDef == "yes" && p.type == "real" && p.rows ≤ 100, ssmc)
-fetch_ssmc(ufl_posdef, format="MM")
+# fetch_ssmc(ufl_posdef, format="MM")
 
 PkgBenchmark.benchmarkpkg("Krylov")
 # include("./Krylov.jl/benchmark/run_benchmarks.jl")
