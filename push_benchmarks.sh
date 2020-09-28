83e20b90f42c324a2b998f7e7076b789e4d5620f
Monssaf Toukal <tmonssaf@gmail.com>
2020-09-28 12:33:37 -0400

    correcting string parsing in sh script

--- push_benchmarks.sh
+++ push_benchmarks.sh
@@ -6,5 +6,5 @@ if julia benchmark/run_benchmarks.jl $bmark_file ; then
 
     julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -g
 else
-    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**An error has occured while running the benchmarks in file $bmark_file** '
+    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "**An error has occured while running the benchmarks in file $bmark_file** "
 fi
